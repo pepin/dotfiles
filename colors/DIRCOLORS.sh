ba340bfe7cccee32aa65b7c8a50c88d0b19aee87
James Pepin <jpepin@llnw.com>
2012-06-21 13:50:18 -0700

    adding in color setup scripts.  I forget exactly where I got this from, but its really helpful in setting up differently colored bash prompts for different servers.

---
+++ colors/DIRCOLORS.sh
@@ -0,0 +1,5 @@
+# Set up colors for this test directory
+
+eval `dircolors --sh $PWD/DIRCOLORS.conf`
+
+# $Id: DIRCOLORS.sh 3 2004-04-15 13:24:41Z scott $
---
+++ colors/setcolors.sh
@@ -0,0 +1,18 @@
+
+if [ $0 != "bash" && $0 != "-bash"]; then
+    echo "This command should be 'sourced' in your bash shell"
+    echo $0
+else
+./colortest -w
+
+echo -ne "What color would you like ? "
+
+read color
+
+echo "You chose $color"
+
+./colortest | grep "[^0-9]$color[^0-9]" | perl -ne '$_ =~ m/([\d]+)\s*:\s*(.)(.)\/(.)(.)\/(.)(.)/; print "#!/bin/sh\n\nexport PROMPT_COLOR=\"38;5;$1\"\nexport CURSOR_COLOR=\"rgb:$2$2$3$3/$4$4$5$5/$6$6$7$7\"\n"' > ~/.bash_prompt_colors
+
+. ~/.bash/prompt
+
+fi
