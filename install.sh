e1151cfef665c1f017414c357d3ec4ed28c631c6
James Pepin <jpepin@llnw.com>
2013-01-11 17:01:31 -0500

    change to installer to work remotely with all new bundles and submodules

--- install.sh
+++ install.sh
@@ -6,13 +6,22 @@
 if [ $# -eq 1 ]; then
     #doing remote install
    copy_cmd="copy_remote"
+   link_cmd="link_remote"
 else
    copy_cmd="copy_local"
+   link_cmd="link_local"
 fi
 git submodule update --init
 server=$1
 function copy_remote(){
     rsync -a --exclude=".git" "$1" "$server:~/"
+}
+function link_remote(){
+    echo "Copyiing $1 to $server:$2"
+    rsync -a --exclude=".git" "$1" "$server:$2"
+}
+function link_local() {
+    ln -s "$1" "$2"
 }
 function copy_local(){
     if [ $(uname) == "Darwin" ]; then
@@ -38,14 +47,19 @@ done
 function add_bundle(){
     dest_path="$HOME/.vim/bundle/$1"
     source_path="$current_dir/vim-bundles/$1"
-    mkdir -p ~/.vim/bundle
-    if [ -e "$dest_path" ]; then
-        echo "Bundle $1 already exists."
-    elif [ -d "$source_path" ]; then
-        echo "Creating link to $dest_path from $source_path"
-        ln -s "$source_path" "$dest_path"
+    if [ "$link_cmd" == "link_remote" ]; then
+            ssh $server mkdir -p "$HOME/.vim/bundle"
+            $link_cmd "$source_path/" "$dest_path"
     else
-        echo "Could not add bundle '$1', was not found in '$current_dir/vim-bundles/'"
+        mkdir -p ~/.vim/bundle
+        if [ -e "$dest_path" ]; then
+            echo "Bundle $1 already exists."
+        elif [ -d "$source_path" ]; then
+            echo "Creating link to $dest_path from $source_path"
+            $link_cmd "$source_path" "$dest_path"
+        else
+            echo "Could not add bundle '$1', was not found in '$current_dir/vim-bundles/'"
+        fi
     fi
 }
 
@@ -54,8 +68,14 @@ add_bundle "camelcasemotion"
 add_bundle "surround"
 add_bundle "python-mode"
 if [ ! -e  "$HOME/.oh-my-zsh" ]; then
-    ln -s "$current_dir/oh-my-zsh" "$HOME/.oh-my-zsh"
+    $link_cmd "$current_dir/oh-my-zsh" "$HOME/.oh-my-zsh"
 fi
 if [ ! -e  "$HOME/.oh-my-zsh-custom" ]; then
-    ln -s "$current_dir/oh-my-zsh-custom" "$HOME/.oh-my-zsh-custom"
+    $link_cmd "$current_dir/oh-my-zsh-custom" "$HOME/.oh-my-zsh-custom"
+fi
+
+if [ "$link_cmd" == "link_remote" ]; then
+    $link_cmd "$current_dir/oh-my-zsh/" "$HOME/.oh-my-zsh"
+    $link_cmd "$current_dir/oh-my-zsh-custom/" "$HOME/.oh-my-zsh-custom"
+    $link_cmd "$current_dir/colors/" "$HOME/.colors"
 fi
