581b5e58939d888a19bfcbee0d06bd5be8063299
James Pepin <jpepin@llnw.com>
2013-01-11 17:00:32 -0500

    fixed colors so they work with all the colors, and not just the 3 digit ones

--- colors/setcolors.sh
+++ colors/setcolors.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-if [ $0 != "bash" && $0 != "-bash"]; then
+if [ $0 != "bash" && $0 != "-bash" ]; then
     echo "This command should be 'sourced' in your bash shell"
     echo $0
 else
-./colortest -w
+    ./colortest -w
 
-echo -ne "What color would you like ? "
+    echo -ne "What color would you like ? "
 
-read color
+    read color
 
-echo "You chose $color"
+    echo "You chose $color"
 
-./colortest | grep "[^0-9]$color[^0-9]" | perl -ne '$_ =~ m/([\d]+)\s*:\s*(.)(.)\/(.)(.)\/(.)(.)/; if ($1 < 100) {$cnum="0$1";}else{$cnum=$1} print "#!/bin/sh\n\nexport PROMPT_COLOR_NUM=\"$cnum\"\nexport PROMPT_COLOR=\"38;5;$1\"\nexport CURSOR_COLOR=\"rgb:$2$2$3$3/$4$4$5$5/$6$6$7$7\"\n"' > ~/.bash_prompt_colors
+    ./colortest | grep '[^0-9]'$color'[^0-9]' | perl -ne '$_ =~ m/([\d]+)\s*:\s*(.)(.)\/(.)(.)\/(.)(.)/; printf("#!/bin/sh\n\nexport PROMPT_COLOR_NUM=\"%0*d\"\nexport PROMPT_COLOR=\"38;5;$1\"\nexport CURSOR_COLOR=\"rgb:$2$2$3$3/$4$4$5$5/$6$6$7$7\"\n", 3, $1);' > ~/.bash_prompt_colors
 
-. ~/.bash/prompt
+    . ~/.bash/prompt
 
 fi
